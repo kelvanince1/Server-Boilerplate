69ad146fa344420e4a1053d543b71f4dd0cf6338
Kelvan Ince <incekelvan@gmail.com>
2017-05-02 19:20:36 -0500

    Boilerplate for server side authorization

---
+++ models/user.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt-nodejs');
+
+// Define the model
+const userSchema = new Schema({
+  email: { type: String, unique: true, lowercase: true },
+  password: String
+});
+
+// On Save Hook, Encrypt Password
+// Before saving a model, run this function
+userSchema.pre('save', function(next) {
+  // Get access to a user model
+  const user = this;
+
+  // Generate a salt, then run a callback.
+  bcrypt.genSalt(10, function(err, salt) {
+    if (err) { return next(err); }
+
+    // Encrypt(hash) password using the salt
+    bcrypt.hash(user.password, salt, null, function(err, hash) {
+      if (err) { return next(err); }
+
+      // Overwrite plain text password with encrypted password
+      user.password = hash;
+      next();
+    });
+  });
+});
+
+userSchema.methods.comparePassword = function(candidatePassword, callback) {
+  bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
+    if (err) { return callback(err)};
+
+    callback(null, isMatch);
+  });
+}
+
+// Create the model class
+const ModelClass = mongoose.model('user', userSchema);
+
+// Export the model
+module.exports = ModelClass;
---
+++ services/passport.js
@@ -0,0 +1,51 @@
+const passport = require('passport');
+const User = require('../models/user');
+const config = require('../config');
+const JwtStrategy = require('passport-jwt').Strategy;
+const ExtractJwt = require('passport-jwt').ExtractJwt;
+const LocalStrategy = require('passport-local');
+
+// Create local Strategy
+const localOptions = { usernameField: 'email' };
+const localLogin = new LocalStrategy(localOptions, function(email, password, done) {
+  // Verify username and password if correct, call done
+  // Otherwise call false
+  User.findOne({ email: email }, function(err, user) {
+    if (err) { return done(err) };
+    if (!user) { return done(null, false) };
+
+    // compare password. Is password == user.password?
+    user.comparePassword(password, function(err, isMatch) {
+      if (err) { return done(err); }
+      if (!isMatch) { return done(null, false); }
+
+      return done(null, user);
+    })
+  })
+})
+
+// Setup options for JWT Strategy
+const jwtOptions = {
+  jwtFromRequest: ExtractJwt.fromHeader('authorization'),
+  secretOrKey: config.secret
+};
+
+// Create JWT Strategy
+const jwtLogin = new JwtStrategy(jwtOptions, function(payload, done) {
+  // See if user id in the paylod exisits the database
+  // If it does, call 'done' with that other <- Done is a passport property
+  // Otherwise, call done without a user object
+  User.findById(payload.sub, function(err, user) {
+    if (err) { return done(err, false); }
+
+    if (user) {
+      done(null, user);
+    } else {
+      done(null, false);
+    }
+  });
+});
+
+// Tell passport to use this strategy
+passport.use(jwtLogin);
+passport.use(localLogin);
